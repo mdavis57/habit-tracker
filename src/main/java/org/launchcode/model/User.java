4d7053e17161eb17bc7b4fe1049565c54aa8fd46
Milena Davis <milenadavis8808@gmail.com>
2019-08-12 12:15:59 -0500

    Added Lombok

--- src/main/java/org/launchcode/model/User.java
+++ src/main/java/org/launchcode/model/User.java
@@ -1,8 +1,16 @@
 package org.launchcode.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.annotation.sql.DataSourceDefinition;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "user")
 @Entity
 public class User {
@@ -13,48 +21,18 @@ public class User {
     private Long id;
 
     @Column(name = "email")
+    @NotNull
     private String email;
 
     @Column(name = "username")
+    @NotNull
+    @Size(min=5, max=15)
     private String username;
 
     @Column(name = "password")
+    @NotNull
+    @Size(min=5, max=15)
     private String password;
 
-    public User(String email, String username, String password) {
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
 
-    public User() { }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
